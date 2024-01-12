cc8022d46c31f4ef57a716c2342790ccb15eb7b2
hulkstark <rawatamit2002@gmail.com>
2024-01-12 16:20:11 +0530

    update resume 2

--- frontend/src/components/Resume.js
+++ frontend/src/components/Resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Resumeimg from './Resumeimg.png';
+import amit from './amit.png';
+import rawat from './rawat.png'
 
 const Resume = () => {
   return (
     <div>
-      <img src={Resumeimg} alt="resume" style={{ height: '1500px', width: '1200px' }} />
+      <img src={amit} alt="resume" style={{ height: '1500px', width: '1200px' }} />
+      <img src={rawat} alt="resume" style={{ height: '1500px', width: '1200px' }} />
     </div>
   );
 };
