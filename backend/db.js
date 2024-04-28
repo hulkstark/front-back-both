ece6adc5b974a272f18f773c4e85b51315c2e8d1
Amit singh rawat <30781152+hulkstark@users.noreply.github.com>
2024-04-28 13:42:47 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect('mongodb+srv://rawatamit2002:MCr7T8jEqUdeF9WD@user-db.spbrwt5.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect('mongodb+srv://rawatamit2002:MCr7T8jEqUdeF9WD@user-db.spbrwt5.mongodb.net/?retryWrites=true&w=majority&appName=user-db')
     console.log("Connected to mongoose successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
