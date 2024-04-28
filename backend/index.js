7d6f17b9e6357408cfab26b8eba2f6135dc6291b
Amit singh rawat <30781152+hulkstark@users.noreply.github.com>
2024-04-28 18:54:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,26 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
-app.use(cors({
-  origin: "*",
+// CORS configuration
+const corsOptions = {
+  origin: "https://front-back-both-client.vercel.app",
   methods: ["POST", "GET"],
   credentials: true,
-  optionsSuccessStatus: 204  // Add this line
-}));
+  optionsSuccessStatus: 204 // No Content
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
+
+// Body parsing middleware
 app.use(express.json());
 
-connectToMongo(); // Connect to MongoDB
-
-
+// Connect to MongoDB
+connectToMongo();
 
 // Define a route handler for the root path
 app.get('/', (req, res) => {
   res.send('Hello, World!');
-  
 });
 
 // Use the auth routes
@@ -29,6 +33,13 @@ app.use("/api/auth", require("./routes/auth"));
 // Other routes can be added similarly
 // app.use("/api/notes", require("./routes/notes"));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
