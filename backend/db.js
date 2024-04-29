3408f0a4b12701cd0616f07c1345eb30ae9a7de4
Amit singh rawat <30781152+hulkstark@users.noreply.github.com>
2024-04-29 21:53:01 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect('MONGO_UR')
+    await mongoose.connect('MONGO_URL')
     console.log("Connected to mongoose successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
